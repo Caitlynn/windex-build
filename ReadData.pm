1e3ead671f5d2e5befb5606b90acef22af64be98
Caitlynn <chow.tian.oo@gmail.com>
2016-05-26 12:44:32 +1000

    move to vm

--- ReadData.pm
+++ ReadData.pm
@@ -2,26 +2,30 @@ use strict;
 use warnings;
 use Exporter;
 
+
+# this method takes a url as input and put the content in the url to a scalar
+#returns the scalar
 sub readURL{
 	my $url = shift;
 	my $ua = LWP::UserAgent->new();
 	$ua->ssl_opts( 'verify_hostname' => 0 );
 	my $res = $ua->get($url);
 	print $res->content;
+	return $res->content;
 }
 
+# this method takes a file and a text that needs to be match as input
+# searching the text in the file and prints out the line with the seaching text
 sub readFile{
 	my $file = shift;
-	unless(open FILE, $file) {
-    	die "Cannot open the file","\n"
+	my $text = shift;
+	open(my $fn, '<', $file) or die "can't open the file '$file'!". "\n";
+	while (my $line = <$fh>){
+		if ($line =~ $text){
+			# need to modify here
+			print $line;
+		}
 	}
-	# Get rid of the line separator.
-	# This allows us to read everything
-	# in one go.
-	undef $/;
-	my $contents = <FILE>;
-	print "Read".length($contents)."bytes","\n";
-	close FILE;
 }
 
 }
--- windex-build.pl
+++ windex-build.pl
@@ -10,6 +10,26 @@ my ($name, $starturl, $excludeFile, $depth , $dir) = validateArgs(@ARGV);
 
 # print "name: '$name' ", "\n", "starturl: '$starturl' ", "\n", "excludeFile: '$excludeFile' ", "\n", "depth : '$depth' ", "\n", "dir: '$dir'" , "\n" ;
 readURL($starturl);
+getLinks($starturl);
+
+sub getLinks {
+	my($startUrl) = shift;
+	my $mech = WWW::Mechanize->new();
+	$mech->get( $startUrl );
+
+	# get the links from that page
+	my @links = $mech->find_all_links();
+	my @viewedArray;
+	for my $link ( @links ) {
+		if ($link->text =~ /([A-Z])\w+/){
+			# WRITE TO FILE IN HERE
+			push @viewedArray, $link->url;
+		}
+	}
+	return @viewedArray
+}
+
+	
 
 
 
